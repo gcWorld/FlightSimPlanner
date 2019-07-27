55bd144ffac429fe158b3a2e65e1f65f20f10152
seeadler <jonas@gcworld.de>
2019-07-27 18:41:00 +0200

    fix comma at end

--- raw/parse.py
+++ raw/parse.py
@@ -10,21 +10,24 @@ f.write('  "type": "FeatureCollection",\r')
 f.write('  "features": [\r')
 
       
-
+txt = ""
 for neighbor in root.iter('Ahp'):
     print(neighbor.find('codeIcao').text, neighbor.find("geoLat").text, neighbor.find("geoLong").text,)
-    f.write('    {\r')
-    f.write('    "type": "Feature",\r')
-    f.write('    "geometry": {\r')
-    f.write('      "type": "Point",\r')
-    f.write('      "coordinates": ['+neighbor.find("geoLong").text[:-1]+', '+neighbor.find("geoLat").text[:-1]+']\r')
-    f.write('    },\r')
-    f.write('    "properties": {\r')  
-    f.write('      "title": "'+neighbor.find('codeIcao').text+'",\r')  
-    f.write('      "icon": "airport",\r')
-    f.write('      "name": "<strong>'+neighbor.find('txtName').text.encode('utf-8').replace('"','')+'</strong>"\r')
-    f.write('    }\r')
-    f.write("},\r")
+    txt += '    {\r'
+    txt += '    "type": "Feature",\r'
+    txt += '    "geometry": {\r'
+    txt += '      "type": "Point",\r'
+    txt += '      "coordinates": ['+neighbor.find("geoLong").text[:-1]+', '+neighbor.find("geoLat").text[:-1]+']\r'
+    txt += '    },\r'
+    txt += '    "properties": {\r'
+    txt += '      "title": "'+neighbor.find('codeIcao').text+'",\r'
+    txt += '      "icon": "airport",\r'
+    txt += '      "name": "<strong>'+neighbor.find('txtName').text.encode('utf-8').replace('"','')+'</strong>"\r'
+    txt += '    }\r'
+    txt += "},\r"
+
+f.write(txt[:-1])
+f.write("\r")
 f.write(' ]\r')
 f.write('}')
 f.close()
@@ -34,21 +37,26 @@ f.write('{\r')
 f.write('  "type": "FeatureCollection",\r')
 f.write('  "features": [\r')
 
+txt = ""
 for neighbor in root.iter('Vor'):
     uid = neighbor.find("VorUid")
     print(uid.find('codeId').text, uid.find("geoLat").text, uid.find("geoLong").text,neighbor.find('txtName').text, neighbor.find("codeType").text, neighbor.find("valFreq").text,)
-    f.write('    {\r')
-    f.write('    "type": "Feature",\r')
-    f.write('    "geometry": {\r')
-    f.write('      "type": "Point",\r')
-    f.write('      "coordinates": ['+uid.find("geoLong").text[:-1]+', '+uid.find("geoLat").text[:-1]+']\r')
-    f.write('    },\r')
-    f.write('    "properties": {\r')  
-    f.write('      "title": "'+neighbor.find('codeType').text+' '+neighbor.find('valFreq').text+'",\r')  
-    f.write('      "icon": "airport",\r')
-    f.write('      "name": "<strong>'+neighbor.find('txtName').text.encode('utf-8').replace('"','')+'</strong>"\r')
-    f.write('    }\r')
-    f.write("},\r")
+    txt += '    {\r'
+    txt += '    "type": "Feature",\r'
+    txt += '    "geometry": {\r'
+    txt += '      "type": "Point",\r'
+    txt += '      "coordinates": ['+uid.find("geoLong").text[:-1]+', '+uid.find("geoLat").text[:-1]+']\r'
+    txt += '    },\r'
+    txt += '    "properties": {\r' 
+    txt += '      "title": "'+uid.find('codeId').text+'",\r'
+    txt += '      "info": "'+neighbor.find('codeType').text+' '+neighbor.find('valFreq').text+'",\r'
+    txt += '      "icon": "airport",\r'
+    txt += '      "name": "<strong>'+neighbor.find('txtName').text.encode('utf-8').replace('"','')+'</strong>"\r'
+    txt += '    }\r'
+    txt += "},"
+
+f.write(txt[:-1])
+f.write("\r")
 
 for neighbor in root.iter('Ndb'):
     uid = neighbor.find("NdbUid")
